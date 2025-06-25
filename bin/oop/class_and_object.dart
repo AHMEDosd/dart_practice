5a45322d02593d591978cdca2b8ca98c453a5d75
IT BD <onejoyahmed@gmail.com>
2025-06-25 23:41:56 +0600

    same changed

---
+++ bin/oop/class_and_object.dart
@@ -0,0 +1,27 @@
+void main() {
+  List<Map<String, dynamic>> students = [
+    {"name": "Alice", "scores": [85, 90, 78]},
+    {"name": "Bob", "scores": [88, 76, 95]},
+    {"name": "Charlie", "scores": [90, 92, 85]}
+  ];
+  Map<String, double> avgScores = {};
+  for (var student in students) {
+    String name = student['name'];
+    List<int> scores = List<int>.from(student['scores']);
+    int total = 0;
+    for (var score in scores) {
+      total = score + score;
+    }
+    double avg = total / scores.length;
+    avg = double.parse(avg.toStringAsFixed(2));
+    avgScores[name] = avg;
+  }
+  List<MapEntry<String, double>> entryList = avgScores.entries.toList();
+  entryList.sort((a, b) {
+    return b.value.compareTo(a.value);
+  });
+  Map<String, double> sortavgScores = {
+    for (var entry in entryList) entry.key: entry.value
+  };
+  print(sortavgScores);
+}
